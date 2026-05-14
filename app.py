68e7bf448d3e25358435bdddff5063fb89659183
Nithin-d-eng <nithinmars1@gmail.com>
2026-05-14 11:27:39 +0530

    final api fix

--- app.py
+++ app.py
@@ -5,10 +5,12 @@ import os
 app = Flask(__name__)
 CORS(app)
 
+# HOME ROUTE
 @app.route('/')
 def home():
     return "API Running ✅"
 
+# PREDICT ROUTE
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
 
@@ -18,9 +20,10 @@ def predict():
     if 'file' not in request.files:
         return "No file uploaded"
 
-    # ✅ TEMP DUMMY RESULT
+    # TEMP RESULT
     return "Disease: Leaf Blight (92.45%)"
 
+# RUN SERVER
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
